1d487e5aa510db2d54e2dfea143ed7f9ba6168e4
egune <egune@ERKANSUNPC>
2025-05-09 21:53:51 +0300

    Add project files.

---
+++ KargoUcretiHesapla/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace KargoUcretiHesapla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Paketiniz kaç kg?: ");
+            double kgAgirlik = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Paketiniz kaç km uzağa gönderilecek?: ");
+            double kmUzaklik = Convert.ToDouble(Console.ReadLine());
+
+            double baslangicFiyat = 100, kgFiyat = 5, kmFiyat = 1;
+
+            double KargoFiyatiHesapla(double kg, double km)
+            {
+                return baslangicFiyat + (kg * kgFiyat) + (km * kmFiyat);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Kargo gönderim ücetiniz: {KargoFiyatiHesapla(kg:kgAgirlik, km: kmUzaklik)} TL'dir");
+
+            Console.ReadKey();
+        }
+    }
+}
